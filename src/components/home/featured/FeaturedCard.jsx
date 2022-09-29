2053cf276ba9ada9d612cc208cab32ba3e3f7f5d
Esam-Ankir <eankir@hotmail.com>
2022-09-29 14:12:00 +0300

    add model filter

--- src/components/home/featured/FeaturedCard.jsx
+++ src/components/home/featured/FeaturedCard.jsx
@@ -1,20 +1,20 @@
 import React, { useContext } from "react"
 import { featured } from "../../data/Data"
 import "./FeaturedCard.css"
-import { postContext } from "../../context/postContext";
+// import { postContext } from "../../context/postContext";
 
 const FeaturedCard = (props) => {
-  const model = useContext(postContext);
+  // const model = useContext(postContext);
 
-  const handleName = (name) => {
-    model.setModel(name)
-  }
+  // const handleName = (name) => {
+  //   model.setModel(name)
+  // }
 
   return (
     <>
       <div className='modelIcon' >
         {featured.map((items, index) => (
-          <div className='box' key={index} onClick={handleName(items.name)}>
+          <div className='box' key={index} >
             <img src={items.cover} alt='' />
             <h4 >{items.name}</h4>
             <label>{items.total}</label>
--- src/components/home/recent/RecentCard.jsx
+++ src/components/home/recent/RecentCard.jsx
@@ -1,84 +1,104 @@
 import React, { useEffect, useState, useContext } from "react"
 // import { list } from "../../data/Data"
 import { LoginContext } from "../../context/context"
-import FeaturedCard from "../featured/FeaturedCard";
+// import FeaturedCard from "../featured/FeaturedCard";
+import { featured } from "../../data/Data"
+import "../featured/FeaturedCard.css"
 // import PostProvider from '../../context/postContext'
+import Logo from "./noImage.png"
+const RecentCard = (props) => {
 
-
-const RecentCard = () => {
-  // const auth = useContext(LoginContext)
-  // const cat = ["lands", "houses"]
+  const auth = useContext(LoginContext)
   const [users, setUsers] = useState([])
+  const [model, setModel] = useState("lands");
   const [images, setImages] = useState([])
+
+  const [postId, setPostId] = useState();
+
+
+
+
+  // console.log("postId", postId);
   const fetchData = async () => {
-    await fetch(`https://akarcom-mid-project.herokuapp.com/lands`)
+    console.log("Data fetched");
+    await fetch(`https://akarcom-mid-project.herokuapp.com/${model}`)
       .then(response => {
         return response.json();
       })
+
       .then(data => {
         setUsers(data)
       })
-    fetchImage()
-  }
 
+  }
+  // /:model/:postId/:modelImages
   const fetchImage = async () => {
-    await fetch("https://akarcom-mid-project.herokuapp.com/lands/11/landImages")
+    await fetch(`https://akarcom-mid-project.herokuapp.com/${model}/${postId}/landImages`)
+
       .then(imgResponse => {
-        // console.log(imgResponse.json())
         return imgResponse.json();
       })
       .then(imgData => {
         setImages(imgData)
       })
   }
-
-
   useEffect(() => {
     fetchData()
-  }, [])
-  // const RecentCard = () => {
-  return (
-    // <div>
+    fetchImage()
+  }
+    , [model])
 
-    //   {users.length > 0 && (
-    //     <ul>
-    //       {users.map(user => (
-    //         <li key={user.id}>{user}</li>
-    //       ))}
-    //     </ul>
-    //   )}
-    // </div>
+
+  return (
+
     <>
-      <FeaturedCard />
+      <div className='modelIcon' >
+        {featured.map((items, index) => (
+          <div className='box' key={index} >
+            <img src={items.cover} alt='' />
+            <h4 >{items.name}</h4>
+            <label>{items.total}</label>
+            <button onClick={() => setModel(items.name)}>
+              Click me!
+            </button>
+          </div>
+
+        ))}
+      </div>
       <br />
       <br />
       <div className='content grid3 mtop'>
         {users.map((val, index) => {
-          const { process, model, owner, price, city } = val
+          const { process, model, owner, price, city, id } = val
           const cover = images.url1;
-          return (
 
-              <div className='box shadow' key={index}>
-                <div className='img'>
-                  <img src={cover} alt='' />
-                </div>
-                <div className='text'>
-                  <div className='category flex'>
-                    <span style={{ background: process === "Sell" ? "#25b5791a" : "#ff98001a", color: process === "Sell" ? "#25b579" : "#ff9800" }}>{process}</span>
-                    <i className='fa fa-heart'></i>
-                  </div>
-                  <h4>{owner}</h4>
-                  <p>
-                    <i className='fa fa-location-dot'></i> {city}
-                  </p>
-                </div>
-                <div className='button flex'>
-                  <div>
-                    <button className='btn2'>{price}</button> <label htmlFor=''>/sqft</label>
-                  </div>
-                  <span>{model}</span>
-                </div>
+          console.log(images.postId);
+          console.log(id);
+          // setPostId(id)
+          //  {fetchImage(id)}
+          return (
+            <div className='box shadow' key={index}>
+              <div className='img'>
+                <img src={cover || Logo} alt='' />
               </div>
+              <div className='text'>
+                <div className='category flex'>
+                  <span style={{ background: process === "Sell" ? "#25b5791a" : "#ff98001a", color: process === "Sell" ? "#25b579" : "#ff9800" }}>{process}</span>
+                  <i className='fa fa-heart'></i>
+                </div>
+                <h4>{owner}</h4>
+                <p>
+                  <i className='fa fa-location-dot'></i> {city}
+                </p>
+              </div>
+              <div className='button flex'>
+                <div>
+                  <button className='btn2'>{price}</button> <label htmlFor=''> JOD</label>
+                </div>
+                <span>{model}</span>
+                <span >post id = {id} </span>
+              </div>
+            </div>
 
           )
         })}
