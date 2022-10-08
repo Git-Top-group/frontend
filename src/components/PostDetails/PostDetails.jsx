a883488e95d0b0a621e23a2bd568099b94e327b5
mohammadsh96 <mhmd.shrydh1996@gmail.com>
2022-10-08 22:45:00 +0300

    contact form + react  notifications

--- src/components/PostDetails/PostDetails.jsx
+++ src/components/PostDetails/PostDetails.jsx
@@ -1,28 +1,26 @@
 import React, { useContext, useEffect, useState } from "react";
-import { baseURL} from "../../utilize/constants";
+import { baseURL } from "../../utilize/constants";
 import noImage from "../images/noImage.png";
 import cookie from "react-cookies";
 import "./PostDetails.css";
 import { useHistory } from "react-router-dom";
 
-import {Redirect ,Switch ,Link} from 'react-router-dom';
-import Swal from 'sweetalert2';
+import { Redirect, Switch, Link } from "react-router-dom";
+import Swal from "sweetalert2";
 
-import {useParams} from 'react-router-dom';
-
+import { useParams } from "react-router-dom";
 
 import { LoginContext } from "../context/context";
 
 const PostDetails = (props) => {
   const auth = useContext(LoginContext);
 
-
-let params = useParams()
-console.log(params)
+  let params = useParams();
+  console.log(params);
   const [post, setPost] = useState({});
   const [images, setImages] = useState([]);
-const [redirect ,setRedirect]=useState(false)
-const history = useHistory();
+  const [redirect, setRedirect] = useState(false);
+  const history = useHistory();
   const getPost = async () => {
     await fetch(
       `${baseURL}/${props.match.params.model}/${props.match.params.id}`
@@ -32,7 +30,6 @@ const history = useHistory();
       })
 
       .then((data) => {
-        
         setPost(data);
         let imag = Object.keys(data).map((key) =>
           key.includes("url") ? data[key] : null
@@ -44,7 +41,6 @@ const history = useHistory();
       });
   };
 
-  
   const deletePost = async () => {
     await fetch(
       `${baseURL}/${props.match.params.model}/${cookie.load("id")}/${
@@ -67,7 +63,6 @@ const history = useHistory();
   };
 
   const orderNow = async () => {
-   
     const message = "here is the message";
     let data = {
       user: {
@@ -84,20 +79,29 @@ const history = useHistory();
       body: JSON.stringify(data),
     })
       .then((response) => {
-        setRedirect(true)
+        setRedirect(true);
         return response;
       })
       .then((data) => {
         console.log("done order");
       });
 
-      Swal.fire(
-        'Order Sent Successfully!',
-        'Please Wait Until Admin Contact You.',
-        'success'
-    )
+    Swal.fire(
+      "Order Sent Successfully!",
+      "Please Wait Until Admin Contact You.",
+      "success"
+    );
   };
-
+  const getDateTime = (dateTime) => {
+    try {
+      dateTime = dateTime.toString().replace("T", " ").replace("Z", "");
+      let date = dateTime.split(" ")[0];
+      let time = dateTime.split(" ")[1].split(".")[0];
+      return date + " " + time;
+    } catch (error) {
+      return "";
+    }
+  };
   const getTrashIcon = () => {
     if (
       cookie.load("actions") !== undefined &&
@@ -105,44 +109,42 @@ const history = useHistory();
       cookie.load("actions")[1] === "CRUD_Users"
     ) {
       return (
-        <i className="deletePostIcon"
+        <i
+          className="deletePostIcon"
           class="fa fa-trash"
           aria-hidden="true"
           style={{ float: "right", marginTop: "20px", cursor: "pointer" }}
           onClick={deletePost}
-        > Delete Post</i>
+        >
+          {" "}
+          Delete Post
+        </i>
       );
     }
   };
   let slideIndex = 1;
-  function plusSlides(n) {
-    showSlides((slideIndex += n));
-  }
-  function currentSlide(n) {
-    showSlides((slideIndex = n));
-  }
-  function showSlides(n) {
+  const automaticShowSlides = () => {
     let i;
     let slides = document.getElementsByClassName("mySlides");
-    if (n > slides.length) {
-      slideIndex = 1;
-    }
-    if (n < 1) {
-      slideIndex = slides.length;
-    }
     for (i = 0; i < slides.length; i++) {
       slides[i].style.display = "none";
     }
+    slideIndex += 1;
+    if (slideIndex > slides.length) {
+      slideIndex = 1;
+    }
     slides[slideIndex - 1].style.display = "block";
-  }
+  };
+
   useEffect(() => {
     getPost();
     window.scrollTo(0, 0);
-    showSlides(slideIndex);
+    let interval = setInterval(() => automaticShowSlides(), 5000);
+    return () => clearInterval(interval);
   }, []);
 
   return (
-    <div className="container">
+    <div className="container post-container-final">
       {getTrashIcon()}
 
       <div>
@@ -151,25 +153,28 @@ const history = useHistory();
             images.map((image, index) => {
               return (
                 <div
-                  class="mySlides fade"
+                  class="mySlides fade-card-carosel"
                   style={{ display: index === 0 ? "block" : "none" }}
                 >
-                  <img src={image} style={{ width: "60%" }} alt=""/>
+                  <img src={image} style={{ width: "60%" }} alt="" />
                 </div>
               );
             })
           ) : (
-            <div class="mySlides fade">
+            <div
+              class="mySlides fade-card-carosel"
+              style={{ display: "block" }}
+            >
               <img src={noImage} style={{ width: "60%" }} alt="" />
             </div>
           )}
 
-          <a class="prev" onClick={() => plusSlides(-1)}>
+          {/* <a class="prev" onClick={() => plusSlides(-1)}>
             &#10094;
           </a>
           <a class="next" onClick={() => plusSlides(1)}>
             &#10095;
-          </a>
+          </a> */}
         </div>
         <br />
       </div>
@@ -177,33 +182,42 @@ const history = useHistory();
       <div className="post-details">
         <ul>
           {Object.keys(post).map((key, index) => {
-            return (
-              <li key={index} className="post-details-item">
-                {post[key] !== null && !key.includes("url") ? (
-                  <>
-                    <strong>{key}:</strong> {post[key].toString()}
-                  </>
-                ) : (
-                  ""
-                )}
+            return post[key] !== null &&
+              !key.includes("url") &&
+              !key.includes("id") &&
+              !key.includes("userId") &&
+              !key.includes("updated") ? (
+              <li
+                key={index}
+                className="post-details-item"
+                style={{ color: "#27ae60" }}
+              >
+                <strong style={{ color: "#000" }}>
+                  {key === "createdAt" ? "Created At" : key}:
+                </strong>{" "}
+                {key === "createdAt"
+                  ? getDateTime(post[key])
+                  : post[key].toString()}
               </li>
-            );
+            ) : null;
           })}
         </ul>
       </div>
-      {auth.loginStatus && ( <>
-      
-        <button onClick={() => orderNow()}>order now</button>
-      </>
-        
-      )} 
+      {auth.loginStatus && auth.user.id != post.userId && post != {} && (
+        <>
+          <button onClick={() => orderNow()}>order now</button>
+        </>
+      )}
 
-      {redirect ? 
-      
-      <Switch>
-        <Redirect from="*" to="/services" > </Redirect>
-      </Switch>
-      : <></>}
+      {redirect ? (
+        <Switch>
+          <Redirect from="*" to="/">
+            {" "}
+          </Redirect>
+        </Switch>
+      ) : (
+        <></>
+      )}
     </div>
   );
 };
